6499b6cff751e3e1386fac00586bfc306bbb0f45
Boris Laevsky <borislaevsky@gmail.com>
2017-04-14 17:12:50 +0300

    fixed untracked files

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import sys
+import requests
 
 if __name__ == "__main__":
     print sys.argv
+for i in range(1,len(sys.argv), 2):
+    r = requests.get("http://www.imdb.com/find?ref_=nv_sr_fn&q=" + sys.argv[i] + "+" + sys.argv[i+1] + "&s=all")
