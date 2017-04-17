e29371c8cfd058e8986324e32ec1ea35bac27ff2
Boris Laevsky <borislaevsky@gmail.com>
2017-04-18 00:49:09 +0300

    added cmd line arguments parcing

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import requests
 
 if __name__ == "__main__":
     print sys.argv
-for i in range(1,len(sys.argv), 2):
-    r = requests.get("http://www.imdb.com/find?ref_=nv_sr_fn&q=" + sys.argv[i] + "+" + sys.argv[i+1] + "&s=all")
-#test
+
+parsed_names = []
+for word in sys.argv[1:]:
+    if word[-1] != ";":
+        parsed_names.append(word)
+    else:
+        word = word.replace(";", "")
+        parsed_names.append(word)
+        output = reduce( lambda x, y: x + "+" + y, parsed_names )
+        r = requests.get("http://www.imdb.com/find?ref_=nv_sr_fn&q=" + output + "&s=all")
+        print r.url
+        output = []
+        parsed_names = []
