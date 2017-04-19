f6e66c0d8d43991fd8d303c2548c1e2660939e33
Boris Laevsky <borislaevsky@gmail.com>
2017-04-19 17:47:53 +0300

    code runs onlu in __main__ now

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import requests
 if __name__ == "__main__":
     print sys.argv
 
-output = ""
-for word in sys.argv[1:]:
-    if word[-1] != ";":
-        output += word + "+"
-    else:
-        output += word[:-1] + "+"
-        r = requests.get("http://www.imdb.com/find?ref_=nv_sr_fn&q=" + output[:-1] + "&s=all")
-        text = r.text.encode('utf-8', 'ignore')
-        print text
-        output = ""
+    output = ""
+    for word in sys.argv[1:]:
+        if word[-1] != ";":
+            output += word + "+"
+        else:
+            output += word[:-1] + "+"
+            r = requests.get("http://www.imdb.com/find?ref_=nv_sr_fn&q=" + output[:-1] + "&s=all")
+            text = r.text.encode('utf-8', 'ignore')
+            print text
+            output = ""
