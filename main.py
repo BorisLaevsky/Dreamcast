44eae2fd7133a7d8499045fa1a41f7d66415232e
Boris Laevsky <borislaevsky@gmail.com>
2017-04-19 17:39:49 +0300

    request data now prints, changed parsed_names on output string

--- main.py
+++ main.py
@@ -4,15 +4,13 @@ import requests
 if __name__ == "__main__":
     print sys.argv
 
-parsed_names = []
+output = ""
 for word in sys.argv[1:]:
     if word[-1] != ";":
-        parsed_names.append(word)
+        output += word + "+"
     else:
-        word = word[:-1]
-        parsed_names.append(word)
-        output = "+".join(parsed_names)
-        r = requests.get("http://www.imdb.com/find?ref_=nv_sr_fn&q=" + output + "&s=all")
+        output += word[:-1] + "+"
+        r = requests.get("http://www.imdb.com/find?ref_=nv_sr_fn&q=" + output[:-1] + "&s=all")
         text = r.text.encode('utf-8', 'ignore')
-        print r.url
-        parsed_names = []
+        print text
+        output = ""
