1dac4e90ee1ff5ce254555d13a12ae6140768e63
BorisLaevsky <borislaevsky@gmail.com>
2017-05-01 23:18:05 +0300

    Removed unnecesary loops, text encoding, fixed printing the actors_urls list multiple times

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 if __name__ == "__main__":
     
     print sys.argv
-    temp_list = []
+    
     list_of_actors_urls = []
     output = ""
     for word in sys.argv[1:]:
@@ -14,16 +14,16 @@ if __name__ == "__main__":
         else:
             output += word[:-1] + "+"
             r = requests.get("http://www.imdb.com/find?ref_=nv_sr_fn&q=" + output[:-1] + "&s=all")
-            text = r.text.encode('utf-8', 'ignore')
+            text = r.text
 	    print r.url
 	    soup = BeautifulSoup(text, "html.parser")
-	    for item in soup.find_all(attrs={'class': 'findResult odd'}):
-                for link in item.find_all('a'):
-                    temp_list.append(link.get('href'))
-            list_of_actors_urls.append(str("http://www.imdb.com" + temp_list[0]))
-            temp_list = [] 
+	    items = soup.find_all(attrs={'class': 'findResult odd'})   
+            link = items[0].find_all('a')
+            actor_url = link[0].get('href')
+            list_of_actors_urls.append(str("http://www.imdb.com" + actor_url))
+            
             output = ""
-        print list_of_actors_urls
+    print list_of_actors_urls
         
         
 
