9dd62f324ffa16aeb7678e421ded544d5ec46fb8
BorisLaevsky <borislaevsky@gmail.com>
2017-05-01 22:33:26 +0300

    Parsing incoming data from site to get list of links for actors pages

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
 import requests
+from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
+    
     print sys.argv
-
+    temp_list = []
+    list_of_actors_urls = []
     output = ""
     for word in sys.argv[1:]:
         if word[-1] != ";":
@@ -12,5 +15,16 @@ if __name__ == "__main__":
             output += word[:-1] + "+"
             r = requests.get("http://www.imdb.com/find?ref_=nv_sr_fn&q=" + output[:-1] + "&s=all")
             text = r.text.encode('utf-8', 'ignore')
-            print text
+	    print r.url
+	    soup = BeautifulSoup(text, "html.parser")
+	    for item in soup.find_all(attrs={'class': 'findResult odd'}):
+                for link in item.find_all('a'):
+                    temp_list.append(link.get('href'))
+            list_of_actors_urls.append(str("http://www.imdb.com" + temp_list[0]))
+            temp_list = [] 
             output = ""
+        print list_of_actors_urls
+        
+        
+
+
