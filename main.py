c5131d0385c1c12bfdd3c3cf144c33647f32899b
BorisLaevsky <borislaevsky@gmail.com>
2017-05-04 17:47:47 +0300

    Reworked main into a few functions

--- main.py
+++ main.py
@@ -7,26 +7,32 @@ if __name__ == "__main__":
     print sys.argv
     
     list_of_actors_urls = []
-    output = ""
-    for word in sys.argv[1:]:
-        if word[-1] != ";":
-            output += word + "+"
-        else:
-            output += word[:-1] + "+"
-            r = requests.get("http://www.imdb.com/find?ref_=nv_sr_fn&q=" + output[:-1] + "&s=all") 
-	    print r.url 
-	    soup = BeautifulSoup(r.text, "html.parser")
-            link = soup.find(attrs = {'class': 'findResult odd'}).find('a').get('href') 
-            list_of_actors_urls.append("http://www.imdb.com" + str(link)) 
-            output = ""
+    		
+    def find_actors_url(list):
+        output = "" 
+        for word in list[1:]: 
+             if word[-1] != ";":
+                 output += word + "+"
+             else:
+                  output += word[:-1] + "+"
+                  r = requests.get("http://www.imdb.com/find?ref_=nv_sr_fn&q=" + output[:-1] + "&s=all") 
+                  print r.url 
+                  soup = BeautifulSoup(r.text, "html.parser")
+                  link = soup.find(attrs = {'class': 'findResult odd'}).find('a').get('href') 
+                  list_of_actors_urls.append("http://www.imdb.com" + str(link)) 
+                  output = ""
+        return list_of_actors_urls
     
-    print list_of_actors_urls
+    print find_actors_url(sys.argv)
+  
         
-    for url in list_of_actors_urls:
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, "html.parser")    
-        links = soup.find(attrs = {'class': 'filmo-category-section'}).find_all('b')
-	for link in links:
-	    print link.get_text(';', strip = True)
-        
+    def find_movie_names(list_of_urls):
+        for url in list_of_actors_urls:
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text, "html.parser")    
+            links = soup.find(attrs = {'class': 'filmo-category-section'}).find_all('b')
+        for link in links:
+            print link.get_text()
+    
+    find_movie_names(find_actors_url(sys.argv))        
        
