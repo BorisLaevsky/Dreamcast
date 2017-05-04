a3f6ea6f00af2e334c124e638a50765fdadbb262
BorisLaevsky <borislaevsky@gmail.com>
2017-05-04 13:58:57 +0300

    Added parsing actor`s page for the names of movies, printing the names

--- main.py
+++ main.py
@@ -16,12 +16,17 @@ if __name__ == "__main__":
             r = requests.get("http://www.imdb.com/find?ref_=nv_sr_fn&q=" + output[:-1] + "&s=all") 
 	    print r.url 
 	    soup = BeautifulSoup(r.text, "html.parser")
-            link = soup.find(attrs = {'class': 'findResult odd'}).find('a').get('href')
-            list_of_actors_urls.append(str("http://www.imdb.com" + link)) 
+            link = soup.find(attrs = {'class': 'findResult odd'}).find('a').get('href') 
+            list_of_actors_urls.append("http://www.imdb.com" + str(link)) 
             output = ""
     
     print list_of_actors_urls
         
+    for url in list_of_actors_urls:
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, "html.parser")    
+        links = soup.find(attrs = {'class': 'filmo-category-section'}).find_all('b')
+	for link in links:
+	    print link.get_text(';', strip = True)
         
-
-
+       
