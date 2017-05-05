cdc75870ff3ad3ee28ef57cd9725682b73d7e5c1
BorisLaevsky <borislaevsky@gmail.com>
2017-05-06 00:10:05 +0300

    Fixed that function getting list of movies took list as the argument which raised an error in find_common_movies function

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
-
-   
-
     		
 def find_actors_url(list):
     list_of_actors_urls = []
@@ -19,35 +16,23 @@ def find_actors_url(list):
               list_of_actors_urls.append("http://www.imdb.com" + str(link)) 
               output = ""
     return list_of_actors_urls
-    
-  
         
-def find_movie_names(list_of_urls): 
+def find_movie_names(url): 
     output_list = []    
-    for url in list_of_urls:
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, "html.parser")    
-        links = soup.find(attrs = {'class': 'filmo-category-section'}).find_all('b')
-        for link in links:	
-          output_list.append(link.get_text()) 
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")    
+    links = soup.find(attrs = {'class': 'filmo-category-section'}).find_all('b')
+    for link in links:	
+      output_list.append(link.get_text()) 
     return output_list            
-
 
 def find_common_movies(list_of_names):
     output = []
     for url in find_actors_url(list_of_names):
-        print url
         output.append(set(find_movie_names(url)))
-    return reduce((lambda x, y: x ^ y), output) 
-
-
+    return list(reduce((lambda x, y: x & y), output)) 
 
 if __name__ == "__main__":
    
     print sys.argv
-    
-#    print find_movie_names(find_actors_url(sys.argv))        
-    
-#    print common_elements([1,2,'asd'], ['asd'])
-
     print find_common_movies(sys.argv)
