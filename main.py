a6e60018c76a255aab29b5e591bb0dd0dbad28a0
BorisLaevsky <borislaevsky@gmail.com>
2017-05-06 22:40:38 +0300

    Added list comprehensions in last 2 functions

--- main.py
+++ main.py
@@ -18,18 +18,15 @@ def find_actors_url(list):
     return list_of_actors_urls
         
 def find_movie_names(url): 
-    output_list = []    
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")    
     links = soup.find(attrs = {'class': 'filmo-category-section'}).find_all('b')
-    for link in links:	
-      output_list.append(link.get_text()) 
+    output_list = [link.get_text() for link in links]                
     return output_list            
 
 def find_common_movies(list_of_names):
     output = []
-    for url in find_actors_url(list_of_names):
-        output.append(set(find_movie_names(url)))
+    output = [set(find_movie_names(url)) for url in find_actors_url(list_of_names)]
     return list(reduce((lambda x, y: x & y), output)) 
 
 if __name__ == "__main__":
